3f2b0cb7d1aff5f5fc7ac28e0b57cc9c1ff2541e
LeeDaeMin <laalmeida@uts.edu.co>
2022-12-14 17:35:07 -0500

    feat (getPeople:/id): agregar endpoint que permite obtener los datos pedidos por ID

--- src/server/endpoints/swapiEndpoints.js
+++ src/server/endpoints/swapiEndpoints.js
@@ -1,14 +1,27 @@
 
-
 const applySwapiEndpoints = (server, app) => {
 
     server.get('/hfswapi/test', async (req, res) => {
         const data = await app.swapiFunctions.genericRequest('https://swapi.dev/api/', 'GET', null, true);
         res.send(data);
+        
     });
 
     server.get('/hfswapi/getPeople/:id', async (req, res) => {
-        res.sendStatus(501);
+
+        const {name, mass, height, homeworld} = await app.swapiFunctions.genericRequest(`https://swapi.dev/api/people/${req.params.id}`, 'GET', null, true);
+        const {name: homeworld_name, url: homeworld_id} = await app.swapiFunctions.genericRequest(homeworld, 'GET', null, true);
+
+        const data = {
+            name,
+            mass,
+            height,
+            homeworld_name,
+            homeworld_id
+        }
+        
+        res.send(data);
+
     });
 
     server.get('/hfswapi/getPlanet/:id', async (req, res) => {
